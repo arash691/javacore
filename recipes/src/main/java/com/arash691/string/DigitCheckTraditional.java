d6bdb4533c65777f2e2163461ae9fe6b2428afe8
a.ariani <a.ariani@bluteam.ir>
2023-10-14 08:58:06 +0330

    string contains only digits

---
+++ recipes/src/main/java/com/arash691/string/DigitCheckTraditional.java
@@ -0,0 +1,23 @@
+package com.arash691.string;
+
+
+import java.util.Objects;
+
+public class DigitCheckTraditional {
+    public boolean containsOnlyDigitsTraditional(String input) {
+        if (input == null) {
+            throw new NullPointerException("Input cannot be null");
+        }
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean containsOnlyDigits(String input) {
+        Objects.requireNonNull(input, "Input cannot be null");
+        return input.chars().allMatch(Character::isDigit);
+    }
+}
---
+++ recipes/src/test/java/com/arash691/string/DigitCheckTraditionalTest.java
@@ -0,0 +1,39 @@
+package com.arash691.string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DigitCheckTraditionalTest {
+
+    private final DigitCheckTraditional digitChecker = new DigitCheckTraditional();
+    @Test
+    public void testContainsOnlyDigitsTraditional_WithDigits() {
+        assertTrue(digitChecker.containsOnlyDigitsTraditional("12345"));
+    }
+
+    @Test
+    public void testContainsOnlyDigitsTraditional_WithNonDigits() {
+        assertFalse(digitChecker.containsOnlyDigitsTraditional("12345a"));
+    }
+
+    @Test
+    public void testContainsOnlyDigitsTraditional_WithNullInput() {
+        assertThrows(NullPointerException.class, () -> digitChecker.containsOnlyDigitsTraditional(null));
+    }
+
+    @Test
+    public void testContainsOnlyDigits_WithDigits() {
+        assertTrue(digitChecker.containsOnlyDigits("67890"));
+    }
+
+    @Test
+    public void testContainsOnlyDigits_WithNonDigits() {
+        assertFalse(digitChecker.containsOnlyDigits("67890b"));
+    }
+
+    @Test
+    public void testContainsOnlyDigits_WithNullInput() {
+        assertThrows(NullPointerException.class, () -> digitChecker.containsOnlyDigits(null));
+    }
+}
